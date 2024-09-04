88bcdb4449fc6a8feed6a9d28b7e71682e768cf9
Matthias Hölzl <tc@xantira.com>
2024-09-04 10:58:44 +0200

    Add intro slides

---
+++ Folien/Python/Completed/Intro/01 Tipps für das Blended-Learning Programm.py
@@ -0,0 +1,78 @@
+// -*- coding: utf-8 -*-
+// %% [markdown]
+//
+// <div style="text-align:center; font-size:200%;">
+//  <b>Tipps für das Blended-Learning Programm</b>
+// </div>
+// <br/>
+// <div style="text-align:center; font-size:120%;">Dr. Matthias Hölzl</div>
+// <br/>
+// <div style="text-align:center;">Coding-Akademie München</div>
+// <br/>
+
+// %% [markdown]
+//
+// # Tipps für das Blended-Learning Programm
+//
+// - Das Blended-Learning Programm ist eine sehr effektive Form des Lernens
+// - Wir haben ein paar Tipps um die Freude am Lernen und die Effektivität des
+//   Kurses zu steigern
+
+// %% [markdown]
+//
+// ## Bleiben Sie am Ball!
+//
+// - Verschieben Sie die Videos nicht auf den letztmöglichen Zeitpunkt!
+// - Sehen Sie nicht alle Videos in einem großen Block an!
+
+// %% [markdown]
+//
+// # Machen Sie Pausen!
+//
+// - Die Inhalte der Videos sind manchmal anspruchsvoll
+// - Es bringt wenig, Videos anzusehen, wenn Sie sich nicht mehr konzentrieren können
+
+// %% [markdown]
+//
+// # Machen Sie Mit!
+//
+// - Tippen Sie den Code selber ein
+// - Experimentieren Sie mit dem Code!
+
+// %% [markdown]
+//
+// # Bearbeiten Sie die Workshops!
+//
+// - Dieser Kurs enthält viele Workshops
+// - Diese Workshops sind ein wesentlicher Teil des Kurses
+// - Ohne Workshops wird es Ihnen schwerfallen, den Stoff anzuwenden
+// - Verwenden Sie die Lösungsvorschläge, wenn Sie nicht weiter wissen
+// - ... aber **nur** wenn Sie nicht weiter wissen!
+
+// %% [markdown]
+//
+// ## Arbeiten Sie zusammen!
+//
+// - Bearbeiten Sie die Workshops in kleinen Teams
+// - Diskutieren Sie den Stoff mit Ihren Kollegen
+
+// %% [markdown]
+//
+// ## Nutzen Sie die Live-Sessions und Foren
+//
+// - Stellen Sie Fragen
+// - Diskutieren Sie Ihre Lösungen
+
+// %% [markdown]
+//
+// ## Richten Sie Ihre Arbeitsumgebung ein!
+//
+// - Installieren Sie eine Java-Umgebung mit der Sie arbeiten können
+// - Laden Sie die Kursmaterialien herunter
+// - Falls möglich verwenden Sie zwei Bildschirme
+
+// %% [markdown]
+//
+// ## Viel Erfolg!
+//
+// Und viel Spaß!
